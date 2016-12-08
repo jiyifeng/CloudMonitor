4f3a9f149f126bfeccc7a6f354b020c4b976b40d
tenyue <tenyue@xx.com>
2016-12-08 21:51:42 +0800

    更改为跨平台python脚本

---
+++ AutoInstall.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Author : https://github.com/tenyue/CloudMonitor
+# 自动部署脚本
+
+if __name__ == '__main__':
+    print 'Please input the website path for CloudMonitor(请输入云探针网站路径：)'
+    print 'Eg: /home/wwwroot/default'
+    webdir = raw_input()
